e6d197e29e30560916f18007b12e6c843c46fdc0
Krish Matta <self@krishxmatta.dev>
2023-04-23 03:51:01 -0400

    Remove clutter

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+(setq inhibit-startup-mesage t) ; Disable startup messages
+(setq inhibit-splash-screen t) ; Disable splash screen
+(setq initial-scratch-message nil) ; Disable scratch message
+
+(scroll-bar-mode -1) ; Disable visible scroll bar
+(tool-bar-mode -1) ; Disable tool bar
+(tooltip-mode -1) ; Disable tooltips
+(menu-bar-mode -1) ; Disable menu bar
+
+(set-fringe-mode 10) ; Add margins
