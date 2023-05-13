f74dbb42b3799bfe6d8f86d792bb0336e40c577b
Krish Matta <self@krishxmatta.dev>
2023-05-12 21:56:26 -0700

    Install doom-modeline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,3 +47,10 @@
   (marginalia-mode))
 (setq marginalia-annotator-registry
   (assq-delete-all 'file marginalia-annotator-registry))
+
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package doom-modeline
+  :init
+  (doom-modeline-mode 1))
