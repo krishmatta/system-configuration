3ecd14a37f6d7bd42581d07a541077b83338c893
Krish Matta <self@krishxmatta.dev>
2023-08-23 12:53:55 -0700

    Improve keybindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,6 +66,17 @@
   :config
   (setq which-key-idle-delay 0.3))
 
+(use-package general
+  :config
+  (general-create-definer krishxmatta/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC"))
+
+(use-package evil
+  :init
+  (evil-mode 1))
+
 ; Line numbers
 (setq display-line-numbers-type 'relative)
 (setq display-line-numbers-current-absolute t)
@@ -82,7 +93,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(amx which-key vertico use-package ox-hugo marginalia doom-modeline all-the-icons)))
+   '(evil general amx which-key vertico use-package ox-hugo marginalia doom-modeline all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
