2e282876f28b6402947e8e14f3ba122a2658d9bf
Krish Matta <self@krishxmatta.dev>
2023-08-23 00:20:40 -0700

    Add custom settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,17 @@
 (dolist (mode '(eshell-mode-hook
 		term-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(amx which-key vertico use-package ox-hugo marginalia doom-modeline all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
