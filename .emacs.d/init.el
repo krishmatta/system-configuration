72a399b471d00c0175798b92bb3ab48aa96e43d3
Krish Matta <self@krishxmatta.dev>
2024-01-09 21:24:18 -0800

    Refactor emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,29 @@
-(setq inhibit-startup-mesage t) ; Disable startup messages
-(setq inhibit-splash-screen t) ; Disable splash screen
-(setq initial-scratch-message nil) ; Disable scratch message
+;;; Basic UI
+(setq inhibit-startup-mesage t)         ; Disable startup messages
+(setq inhibit-splash-screen t)          ; Disable splash screen
+(setq initial-scratch-message nil)      ; Disable scratch message
 
-(scroll-bar-mode -1) ; Disable visible scroll bar
-(tool-bar-mode -1) ; Disable tool bar
-(tooltip-mode -1) ; Disable tooltips
-(menu-bar-mode -1) ; Disable menu bar
+(scroll-bar-mode -1)                    ; Disable visible scroll bar
+(tool-bar-mode -1)                      ; Disable tool bar
+(tooltip-mode -1)                       ; Disable tooltips
+(menu-bar-mode -1)                      ; Disable menu bar
 
-(set-fringe-mode 10) ; Add margins
+(set-fringe-mode 10)                    ; Add margins
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Enable escape for quit
+;; Set font
+(set-face-attribute 'default nil :font "Iosevka" :height 150)
 
-(setq auto-save-default nil) ; Disable auto-save
+(load-theme 'grayscale t)               ; Set theme
 
-(set-face-attribute 'default nil :font "Iosevka" :height 150) ; Set font
+;;; Remove Unnecessary Files
+(setq auto-save-default nil)            ; Disable auto-save
+(setq make-backup-files nil)            ; Disable backup files
 
-(load-theme 'grayscale t) ; Set theme
-
-(setq make-backup-files nil) ; Disable backup files
-
-(add-hook 'after-change-major-mode-hook
-          #'(lambda ()
-             (setq-default indent-tabs-mode nil)
-             (setq tab-width 3)))
-
+;;; Package Management
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -37,25 +34,39 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;;; Completion
 (use-package vertico
-  :bind (:map vertico-map
-	 ("C-j" . vertico-next)
-	 ("C-k" . vertico-previous))
+  :bind
+  (:map vertico-map
+        ("C-j" . vertico-next)
+        ("C-k" . vertico-previous))
   :custom
   (vertico-cycle t)
   :init
   (vertico-mode))
 
+;; Persist history over Emacs restarts, so Vertico can sort by history position
 (use-package savehist
   :init
   (savehist-mode))
 
+;; Use orderless completion style
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Minibuffer annotations
 (use-package marginalia
   :init
   (marginalia-mode))
+
+;; Disable file annotations
 (setq marginalia-annotator-registry
   (assq-delete-all 'file marginalia-annotator-registry))
 
+;;; UI
 (use-package all-the-icons
   :if (display-graphic-p))
 
@@ -63,9 +74,19 @@
   :init
   (doom-modeline-mode 1))
 
-(use-package ox-hugo
-  :after ox)
+;; Line numbers
+(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-current-absolute t)
+(global-display-line-numbers-mode)
+(column-number-mode)
 
+(dolist (mode '(eshell-mode-hook term-mode-hook)) (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;; Keybinds
+;; Enable escape for quit
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Shows available keybinds
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
@@ -85,77 +106,14 @@
   :config
   (evil-collection-init))
 
-(use-package org-roam
-  :config
-  (setq org-roam-directory "~/org/roam"))
-
-(use-package org-download
-  :after org
-  :config
-  (org-download-enable))
-
-; Line numbers
-(setq display-line-numbers-type 'relative)
-(setq display-line-numbers-current-absolute t)
-(global-display-line-numbers-mode)
-(column-number-mode)
-
-(dolist (mode '(eshell-mode-hook
-		term-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-roam compat bind-key transient tomelr s dash f shrink-path org org-edna org-agenda-property org-gtd nerd-icons goto-chg annalist evil-collection evil general amx which-key vertico use-package ox-hugo marginalia doom-modeline all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; GTD Setup
-(setq org-capture-templates '(("t" "Inbox" entry (file+headline "~/org/todo/inbox.org" "Inbox") "* TODO %i%?")))
-(setq org-agenda-files '("~/org/todo"))
-
-(defvar org-refile-targets-original (if (boundp 'org-refile-targets)
-					org-refile-targets
-				      nil))
-
-(defun set-refile-target-todo ()
-  (if (and buffer-file-name
-	     (string-prefix-p (expand-file-name "~/org/todo/") buffer-file-name))
-    (setq org-refile-targets
-	  '(("~/org/todo/next.org" :level . 1)
-	    ("~/org/todo/projects.org" :maxlevel . 3)
-            ("~/org/todo/delegated.org" :level . 1)
-            ("~/org/todo/reminders.org" :level . 1)
-            ("~/org/todo/calendar.org" :level . 1)
-            ("~/org/todo/potential.org" :level . 1)))
-    (setq org-refile-targets org-refile-targets-original)))
-  
-
-(add-hook 'post-command-hook 'set-refile-target-todo)
-
+;; Org keybindings
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c a") 'org-agenda)
-(defun find-todo ()
+(defun krishxmatta/find-todo ()
   (interactive)
   (let ((default-directory "~/org/todo/"))
     (call-interactively 'find-file)))
-(global-set-key (kbd "C-c t") 'find-todo)
-
-(defun agenda-todos ()
-  (interactive)
-  (let ((org-agenda-files '("~/org/todo/next.org"
-                            "~/org/todo/projects.org")))
-    (org-todo-list)))
-
-(global-set-key (kbd "C-c C-a t") 'agenda-todos)
+(global-set-key (kbd "C-c t") 'krishxmatta/find-todo)
 
 ;; Evil keybindings
 (defun evil-keyboard-quit ()
@@ -173,12 +131,49 @@
   (define-key org-agenda-mode-map "j" 'evil-next-line)
   (define-key org-agenda-mode-map "k" 'evil-previous-line))
 
-; Roam keybindings
+;; Roam keybindings
 (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
 (global-set-key (kbd "C-c n f") 'org-roam-node-find)
 (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
 
-; Notes
+;;; Org Mode
+(use-package ox-hugo
+  :after ox)
+
+(use-package org-roam
+  :config
+  (setq org-roam-directory "~/org/roam"))
+
+;; Easy image manipulation
+(use-package org-download
+  :after org
+  :config
+  (org-download-enable))
+
+;;;; Task Management Setup
+(setq org-capture-templates '(("t" "Inbox" entry (file+headline "~/org/todo/inbox.org" "Inbox") "* TODO %i%?")))
+(setq org-agenda-files '("~/org/todo"))
+
+(defvar krishxmatta/org-refile-targets-original
+  (if (boundp 'org-refile-targets)
+    org-refile-targets
+    nil))
+
+(defun krishxmatta/set-refile-target-todo ()
+  (if (and buffer-file-name
+    (string-prefix-p (expand-file-name "~/org/todo/") buffer-file-name))
+    (setq org-refile-targets
+      '(("~/org/todo/next.org" :level . 1)
+        ("~/org/todo/projects.org" :level . 1)
+        ("~/org/todo/delegated.org" :level . 1)
+        ("~/org/todo/reminders.org" :level . 1)
+        ("~/org/todo/calendar.org" :level . 1)
+       ("~/org/todo/potential.org" :level . 1)))
+    (setq org-refile-targets krishxmatta/org-refile-targets-original)))
+
+(add-hook 'post-command-hook 'krishxmatta/set-refile-target-todo)
+
+;;;; Note-taking Setup
 (setq org-roam-capture-templates
       '(("m" "main" plain
          "%?"
@@ -204,6 +199,12 @@
 
 (add-hook 'org-roam-capture-new-node-hook #'krishxmatta/org-roam-tag-new-node-as-draft)
 
-(setq org-roam-node-display-template ; Enable search by tag
-      (concat "${title} "
-              (propertize "${tags}" 'face 'org-tag)))
+;; Enable search by tag
+(setq org-roam-node-display-template
+      (concat "${title} " (propertize "${tags}" 'face 'org-tag)))
+
+;;; Customize interface
+(custom-set-variables
+ '(package-selected-packages
+   '(orderless which-key vertico use-package pdf-tools ox-hugo org-roam org-gtd org-download marginalia general evil-collection doom-modeline amx all-the-icons)))
+(custom-set-faces)
