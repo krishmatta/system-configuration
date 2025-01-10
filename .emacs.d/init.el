21a2280b779e855539760ddd8f0a52be9eb110df
Krish Matta <self@krishmatta.net>
2025-01-09 20:51:19 -0800

    Install vterm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -278,6 +278,9 @@
 ;;; Magit
 (use-package magit)
 
+;;; Terminal Emulator
+(use-package vterm)
+
 ;;; Customize interface
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -285,7 +288,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit ein-notebook ein quelpa pdf-continuous-scroll-mode pdf-view-restore org-roam-ui websocket elfeed ewal-evil-cursors ewal-spacemacs-themes ewal orderless which-key vertico use-package pdf-tools ox-hugo org-roam org-gtd org-download marginalia general evil-collection doom-modeline amx all-the-icons)))
+   '(vterm magit-section nerd-icons ein-notebook ein quelpa pdf-continuous-scroll-mode pdf-view-restore org-roam-ui websocket elfeed ewal-evil-cursors ewal-spacemacs-themes ewal orderless which-key vertico use-package pdf-tools ox-hugo org-roam org-gtd org-download marginalia general evil-collection doom-modeline amx all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
